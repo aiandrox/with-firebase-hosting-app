17fe60b7a5126d702247ae2bc2c54ea8d6a76832
Aiandrox <aiandrox7@gmail.com>
2021-01-13 08:07:25 +0900

    クエリから値を取得してHTMLを返す http://127.0.0.1:5000/?title=hoge&text=foo

--- firebaseFunctions.js
+++ firebaseFunctions.js
@@ -14,5 +14,36 @@ const nextjsServer = next({
 const nextjsHandle = nextjsServer.getRequestHandler();
 
 exports.nextjsFunc = https.onRequest((req, res) => {
+  const title = req.query.title;
+  const text = req.query.text;
+  try {
+    res.set("Cache-Control", "public, max-age=600, s-maxage=600");
+    const html = createHtml(title, text);
+    res.status(200).end(html);
+  } catch (error) {
+    res.status(404).end("404 Not Found");
+  }
   return nextjsServer.prepare().then(() => nextjsHandle(req, res));
 });
+
+const createHtml = (title, text) => {
+  const SITEURL = "https://with-firebase-hosting-ap-f53e2.web.app/";
+  return `<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <title>${title}</title>
+    <meta property="og:title" content="${title}">
+    <meta property="og:image" content="${SITEURL}">
+    <meta property="og:description" content="${text}">
+    <meta property="og:url" content="${SITEURL}">
+    <meta property="og:type" content="article">
+    <meta property="og:site_name" content="${title}">
+  </head>
+  <body>
+    あああ
+  </body>
+</html>
+`;
+};
